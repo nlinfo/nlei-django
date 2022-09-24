0407af8e6b7876cc6ad1749579ab5fb5eacd8ba3
Edivaldo <edivaldoluisb16@gmail.com>
2022-09-24 16:07:03 +0100

    Update settings.py

--- nlei/settings.py
+++ nlei/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rb$efk*_1a1dpp=*nqgtjg8qlgkm_(zyk-6z=rh0r@uaod!9k%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # False for deploy
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
