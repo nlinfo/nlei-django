6e8d60b816e0d12fbcd171444e3cd2c9d7d5dc64
Edivaldo <edivaldoluisb16@gmail.com>
2022-10-05 23:44:21 +0100

    Update settings.py

--- nlei/settings.py
+++ nlei/settings.py
@@ -29,7 +29,7 @@ DEBUG = False  # False for deploy
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://nlei.up.railway.app/', 'http://127.0.0.1:8000']
 # HTTPS settings
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
