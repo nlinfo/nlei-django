29e7eb5618d0059985a7da4a4d487b6268e402e8
Edivaldo <edivaldoluisb16@gmail.com>
2022-10-09 17:15:44 +0100

    change postgres to sqllite

--- nlei/settings.py
+++ nlei/settings.py
@@ -101,14 +101,9 @@ WSGI_APPLICATION = 'nlei.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'bpmcJIQKEadtsRdyoZqA',
-        'HOST': 'containers-us-west-54.railway.app',
-        'PORT': '5744',
+        # sqlite3
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
